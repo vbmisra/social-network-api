00cf2ba1c71bc2b12bdfd7288efbe5667b2837ff
Invincible <87626336+vbmisra@users.noreply.github.com>
2022-01-28 22:36:37 -0800

    added user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+})
+
+const User = model('user', userSchema)
+
+module.exports = User
