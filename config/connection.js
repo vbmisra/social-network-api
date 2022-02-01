ee9723f84bd256dc3e06af8641be4396436e1542
Invincible <87626336+vbmisra@users.noreply.github.com>
2022-01-31 18:19:55 -0800

    changed server.js to index.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose')
 
 const connectionString = 
-    process.env.MONGODB_URI || 'mondodb://localhost/usersAndThoughts'
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/usersAndThoughts'
 
 connect(connectionString, {
     useNewUrlParser: true,
